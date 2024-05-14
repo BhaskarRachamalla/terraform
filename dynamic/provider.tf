90c89a4bdd3402c8d04cde34668a20091ab16931
Bhaskar Rachamalla <bhaskar.apr30@gmail.com>
2024-05-14 19:27:05 +0530

    dynamic

---
+++ dynamic/provider.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.48.0"
+    }
+  }
+
+
+  backend "s3" {
+    bucket = "bhaskardevops"
+    key    = "dynamic"
+    region = "us-east-1"
+    dynamodb_table = "terraform-locking"
+  }
+
+}
+
+
+
+provider "aws" {
+  region = "us-east-1"
+}
